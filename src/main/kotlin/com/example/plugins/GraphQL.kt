f31f2a6dd3b633ec741d741c00c368e81108053b
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2021-08-23 06:21:41 +0900

    Add StarWars sample

--- src/main/kotlin/com/example/plugins/GraphQL.kt
+++ src/main/kotlin/com/example/plugins/GraphQL.kt
@@ -1,17 +1,31 @@
 package com.example.plugins
 
 import com.apurebase.kgraphql.GraphQL
+import com.example.domain.Droid
+import com.example.domain.Episode
+import com.example.domain.Human
 import io.ktor.application.*
 
 fun Application.configureGraphQL() {
     install(GraphQL) {
         playground = true
         schema {
-            query("Hello") {
-                resolver { ->
-                    "World"
+            val luke = Human("2000", "Luke Skywalker", emptyList(), Episode.values().toSet(), "Tatooine", 1.72)
+            val r2d2 = Droid("2001", "R2-D2", emptyList(), Episode.values().toSet(), "Astromech")
+            query("hero") {
+                resolver { episode: Episode ->
+                    when (episode) {
+                        Episode.NEWHOPE, Episode.JEDI -> r2d2
+                        Episode.EMPIRE -> luke
+                    }
                 }
             }
+            query("heroes") {
+                resolver { -> listOf(luke, r2d2)}
+            }
+            type<Droid>()
+            type<Human>()
+            enum<Episode>()
         }
     }
 }
