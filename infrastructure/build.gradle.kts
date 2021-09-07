d8907784bf9fcdd2913a8b882a3b982b4da7b616
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2021-09-07 22:44:00 +0900

    Use Koin for DI

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -2,6 +2,7 @@ val kotlin_version: String by project
 val ktor_version: String by project
 val logback_version: String by project
 val kgraphql_version: String by project
+val koin_version: String by project
 
 plugins {
     application
@@ -25,6 +26,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("com.apurebase:kgraphql:$kgraphql_version")
     implementation("com.apurebase:kgraphql-ktor:$kgraphql_version")
+    implementation("org.koin:koin-ktor:$koin_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 }
